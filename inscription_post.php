e2710a905fb888eb892b9967c66d3a67dedb8e0d
Thomas Claireau <thomas.claireau@gmail.com>
2019-01-29 12:10:40 +0100

    vérification pseudo/email/pass avant insertion bdd OK

--- inscription_post.php
+++ inscription_post.php
@@ -8,15 +8,39 @@ catch (Exception $e) {
     die ('Erreur : ' . $e->getMessage());
 };
 
-$pass_hache = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$pseudo = $_POST['pseudo'];
+$pass = $_POST['password'];
+$pass_confirm = $_POST['password_confirmation'];
+$email = $_POST['email'];
 
-$reponse = $bdd->prepare('INSERT INTO espace_membre(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, NOW())');
-$reponse->execute(array(
-    'pseudo' => $_POST['pseudo'],
-    'pass' => $pass_hache,
-    'email' => $_POST['email']
-));
+$reponse = $bdd->query('SELECT pseudo FROM espace_membre');
+$donnee=$reponse->fetch();
 
-header('Location: connexion.php');
+    if ($pseudo !== $donnee['pseudo']) {
+            if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email)) {
+                if ($pass === $pass_confirm) {
+                    $pass_hache = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+                    $reponse->closeCursor();
+
+                    $reponse = $bdd->prepare('INSERT INTO espace_membre(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, NOW())');
+                    $reponse->execute(array(
+                        'pseudo' => $_POST['pseudo'],
+                        'pass' => $pass_hache,
+                        'email' => $_POST['email']
+                    ));
+
+                    header('Location: connexion.php');
+                } else {
+                    echo 'Les deux mots de passe ne sont pas identique';
+                }
+            } else {
+                echo 'Format d\'email non valide';
+            }
+    } else {
+        echo 'Ce pseudo existe déja';
+    }
+
+
 
 ?>
